7684612a608c37038046a281b84468fb5a7a5f47
PezeM <pezemos@gmail.com>
2019-05-17 15:32:36 +0200

    Dodano login window
    Dodano bootstrapa do vue

--- client-ui/src/router.js
+++ client-ui/src/router.js
@@ -14,13 +14,14 @@ export default new Router({
             component: Home
         },
         {
+            path: '/login',
+            name: 'login',
+            component: () => import('./views/Login/Login.vue')
+        },
+        {
             path: '/about',
             name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "about" */ './views/About.vue')
+            component: () => import('./views/About.vue')
         }
     ]
 });
---
+++ client-ui/src/views/Login/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <login-window></login-window>
+    </div>
+</template>
+
+<script>
+import LoginWindow from '@/components/Login/LoginWindow.vue';
+
+export default {
+    name: 'login',
+    components: {
+        LoginWindow
+    }
+};
+</script>

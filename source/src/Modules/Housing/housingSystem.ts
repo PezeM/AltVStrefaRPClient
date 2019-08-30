82bf6b9d0ad9113aec99c0ce57a46659e9dc492f
PezeM <16854655+PezeM@users.noreply.github.com>
2019-08-30 02:48:52 +0200

    Zmieniono housing system

--- source/src/Modules/Housing/housingSystem.ts
+++ source/src/Modules/Housing/housingSystem.ts
@@ -1,23 +1,32 @@
 import * as alt from 'alt';
-import * as native from 'natives';
 import mainUi from 'source/src/Modules/Ui/mainUi';
 import { NotificationTypes } from 'source/src/Constans/notificationTypes';
+import sounds from 'source/src/Modules/Core/soundsController';
 
 const localPlayer = alt.Player.local;
-let insideHouseDoorColshape = false;
-alt.onServer('inHouseEnterColshape', onInHouseEnterColshape);
-
-function onInHouseEnterColshape(entered: boolean) {
-    if (entered) {
-        insideHouseDoorColshape = true;
-        mainUi.showCefNotification(NotificationTypes.Info, "Mieszkanie", "Naciśnij [E] aby otworzyć menu mieszkania", 3000, "house");
-    } else {
-        insideHouseDoorColshape = false;
-    }
-}
 
 class HousingSystem {
+    insideHouseDoorColshape: boolean = false;
 
+    constructor() {
+        alt.onServer('inHouseEnterColshape', this.inHouseEnterColshape.bind(this));
+        alt.onServer('successfullyToggledHouseLock', this.successfullyToggledHouseLock.bind(this));
+    }
+
+    inHouseEnterColshape(entered: boolean) {
+        if (entered) {
+            this.insideHouseDoorColshape = true;
+            mainUi.showCefNotification(NotificationTypes.Info, "Mieszkanie", "Naciśnij [E] aby otworzyć menu mieszkania", 3000, "house");
+        } else {
+            this.insideHouseDoorColshape = false;
+        }
+    }
+
+    successfullyToggledHouseLock(isLocked: boolean) {
+        mainUi.showCefNotification(NotificationTypes.Info, "Zamek", `Pomyślnie ${isLocked ? 'zamknięto' : 'otwarto'} mieszkanie`);
+        // Play some sound on lock/unlock
+        sounds.playCefSound('houseDoorLock.mp3', 0.5, false);
+    }
 }
 
 const housingSystem = new HousingSystem();

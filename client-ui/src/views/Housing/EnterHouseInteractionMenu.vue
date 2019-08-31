7f5e96669d8a8963d12cdc27b2643c0d6ee09f9a
PezeM <16854655+PezeM@users.noreply.github.com>
2019-08-31 22:43:50 +0200

    Layout enter house interaction menu

--- client-ui/src/views/Housing/EnterHouseInteractionMenu.vue
+++ client-ui/src/views/Housing/EnterHouseInteractionMenu.vue
@@ -1,12 +1,43 @@
 <template>
     <div id="enter-house-interaction-menu">
-        <div class="enter-house-interaction-menu container"></div>
+        <div class="enter-house-interaction-menu container">
+            <div class="row menu-header">
+                <div class="col">
+                    <p class="mt-1 mb-1 menu-header-text">{{ getHouseTypeName }}</p>
+                </div>
+            </div>
+            <div class="row enter-house-interaction-menu-container">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col text-center">
+                            <img :src="getInteriorImage" class="img-fluid" />
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col">
+                            <h5 class="text-uppercase">Ulica</h5>
+                            <p class="font-italic">{{ house.streetName }}</p>
+                        </div>
+                    </div>
+                    <buy-button
+                        :price="house.price"
+                        :buttonText="house.houseType == 1 ? 'KUP' : 'WYNAJMIJ'"
+                    />
+                </div>
+                <div class="col-6">Col 2</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import BuyButton from '@/components/Housing/BuyButton.vue';
+
 export default {
     name: 'enterHouseInteractionMenu',
+    components: {
+        BuyButton,
+    },
     props: {
         house: {
             type: Object,
@@ -18,8 +49,17 @@ export default {
                     interiorName: 'Małe mieszkanie',
                     owner: false,
                     isClosed: true,
+                    streetName: 'Dłuższa nazwa ulicy',
                 };
             },
+        },
+    },
+    computed: {
+        getHouseTypeName() {
+            return this.house.houseType == 1 ? 'Mieszkanie' : 'Hotel';
+        },
+        getInteriorImage() {
+            return require('@/assets/images/townHallLogo.png');
         },
     },
 };
@@ -27,6 +67,7 @@ export default {
 
 <style scoped>
 #enter-house-interaction-menu {
+    background-image: url('../../assets/example-image.jpg');
     padding: 0;
     margin: 0;
     display: flex;
@@ -36,13 +77,31 @@ export default {
 }
 
 .enter-house-interaction-menu {
-    width: 40rem;
-    height: 30rem;
+    max-width: 40vh;
     position: relative;
-    background-color: #f8f9fa;
-    color: rgb(0, 0, 0);
+    background-color: #000000e3;
+    color: #f3f3f3;
     overflow: hidden;
     border-radius: 6px;
     box-shadow: 0 0 15px 8px rgba(0, 0, 0, 0.25);
+}
+
+.enter-house-interaction-menu-container {
+    padding: 1rem;
+}
+
+.enter-house-interaction-menu p {
+    font-family: 'Roboto';
+}
+
+.menu-header {
+    background-color: #f8f8f8;
+    box-shadow: 0 0 15px 6px rgba(0, 0, 0, 0.5);
+}
+
+.menu-header-text {
+    font-family: 'Roboto' !important;
+    color: #333333;
+    font-size: 1.3rem;
 }
 </style>

0bdf511d8998953e4e1c9ba621f7b0c0ff4a85c4
PezeM <16854655+PezeM@users.noreply.github.com>
2019-09-06 22:24:58 +0200

    Dodano togglowanie noclipa

--- client-ui/src/views/AdminMenu/AdminDebugMenu.vue
+++ client-ui/src/views/AdminMenu/AdminDebugMenu.vue
@@ -1,17 +1,40 @@
 <template>
     <div class="admin-debug-menu">
-        <h1>Admin debug menu</h1>
+        <h2>Debug mode</h2>
         <div class="row">
             <div class="col-3">
                 <div class="card border shadow">
                     <div class="card-body">
-                        <p
-                            class="card-text"
-                        >Debug mode: {{ debugModeStatus ? 'włączony' : 'wyłączony' }}</p>
+                        <p class="card-text font-weight-bold">
+                            Debug mode:
+                            <span
+                                class="disabled"
+                                :class="{active: debugModeStatus}"
+                            >{{ debugModeStatus ? 'włączony' : 'wyłączony' }}</span>
+                        </p>
                         <button
-                            class="btn btn-primary"
+                            class="btn btn-success"
+                            :class="{'btn-danger': debugModeStatus}"
                             @click="toggleDebugMode"
                         >{{ debugModeStatus ? 'Wyłącz' : 'Włącz' }}</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card border shadow">
+                    <div class="card-body">
+                        <p class="card-text font-weight-bold">
+                            Noclip:
+                            <span
+                                class="disabled"
+                                :class="{active: noclipStatus}"
+                            >{{ noclipStatus ? 'włączony' : 'wyłączony' }}</span>
+                        </p>
+                        <button
+                            class="btn btn-success"
+                            :class="{'btn-danger': noclipStatus}"
+                            @click="toggleDebugMode"
+                        >{{ noclipStatus ? 'Wyłącz' : 'Włącz' }}</button>
                     </div>
                 </div>
             </div>
@@ -24,10 +47,12 @@ export default {
     name: 'adminDebugMenu',
     mounted() {
         alt.on('adminMenu::toggledDebugMode', this.toggledDebugMode);
+        alt.on('adminMenu::toggledNoclipMode', this.toggleNoclipMode);
     },
     data() {
         return {
             debugModeStatus: false,
+            noclipStatus: false,
         };
     },
     methods: {
@@ -37,9 +62,22 @@ export default {
         toggledDebugMode(debugStatus) {
             this.debugModeStatus = debugStatus;
         },
+        toggleNoclipMode() {
+            alt.emit('adminMenu::toggleNoclipMode');
+        },
+        toggledNoclipMode(noclipStatus) {
+            this.noclipStatus = noclipStatus;
+        },
     },
 };
 </script>
 
-<style>
+<style scoped>
+.disabled {
+    color: red;
+}
+
+.active {
+    color: green;
+}
 </style>
